d7f38af2b72770477bdb662ade96a9459ffe3a22
Amjad <amjadmasood2007@gmail.com>
2020-12-27 15:10:08 -0600

    Initial commit - tested

---
+++ sample.py
@@ -0,0 +1,15 @@
+import yagmail
+
+try:
+    for x in range(1):
+        yagmail.register('felefwlab', 'franklinelectric')
+        yag = yagmail.SMTP('felefwlab')
+        to = 'amjadmasood2007@gmail.com'
+        subject = 'This is obviously the subject'
+        body = 'This is obviously the body'
+        html = '<a href="https://pypi.python.org/pypi/sky/">Click me!</a>'
+        img = './email.png'
+        yag.send(to = to, subject = subject, contents = [body, html, img])
+        print("email sent successfully...")
+except:
+    print("something bad happened...")
